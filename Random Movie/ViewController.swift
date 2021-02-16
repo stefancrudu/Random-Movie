0277d7896c5e70efe2a47526d79e552641c03173
Stefan Crudu <stefancrudu@icloud.com>
2021-02-16 21:13:29 +0200

    Initial Commit

---
+++ Random Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Movie
+//
+//  Created by Stefan Crudu on 16.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
